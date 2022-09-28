75d8aefe54333bce2062d1e361fe3b559eb46b6d
vyctoria karina <vyctoriak@gmail.com>
2022-09-28 23:07:27 +0200

    flag no script dev para atualizar frequentemente, flag default id no model game e Ad, retorno da requisicao game, ads do game e discord do autor do ad

--- src/server.ts
+++ src/server.ts
@@ -1,32 +1,73 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
 
 const app = express()
+const prisma = new PrismaClient({
+  log: ['query']
+})
 
-app.get('/games', (request, response) => {
-  return response.json([])
+app.get('/games', async (request, response) => {
+  const games = await prisma.game.findMany({
+    include: {
+      _count: {
+        select: {
+          ads: true
+        }
+      }
+    }
+  })
+
+  return response.json([games])
 })
 
 app.post('/ads', (request, response) => {
   return response.status(201).json([])
 })
 
-app.get('/game/:id/ads', (request, response) => {
+app.get('/game/:id/ads', async (request, response) => {
+  const gameId = request.params.id;
 
-  return response.json([
-    {
-      id: 1,
-      nome: 'anuncio 1'
+  const ads = await prisma.ad.findMany({
+    select: {
+      id: true,
+      name: true,
+      weekDays: true,
+      useVoiceChannel: true,
+      yearsPlaying: true,
+      hourStart: true,
+      hourEnd: true,
     },
-    {
-      id: 2,
-      nome: 'anuncio 2'
+    where: {
+      gameId,
+    },
+    orderBy: {
+      createAt: 'desc'
     }
-  ])
+  })
+
+  return response.json(ads.map((ad) => {
+    return {
+      ...ad,
+      weekDays: ad.weekDays.split(',')
+     }
+  }))
 })
 
-app.get('/ads/:id/discord', (request, response) => {
+app.get('/ads/:id/discord', async (request, response) => {
+  const adId = request.params.id;
 
-  return response.json([])
+  const ad = await prisma.ad.findUniqueOrThrow({
+    select: {
+      discord: true
+    },
+    where: {
+      id: adId,
+    }
+  })
+
+  return response.json({
+    discord: ad.discord
+  })
 })
 
 app.listen(3333)
